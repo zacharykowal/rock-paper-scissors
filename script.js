7691a5e503d018e08d5869f0d5729c196de3098b
Zachary Kowal <zacharykowal31@gmail.com>
2025-05-26 10:43:21 -0400

    Create computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+    if ((randomNumber >= 0) && (randomNumber < 0.33)) {
+        return "rock";
+    } else if ((randomNumber >= 0.33) && (randomNumber < 0.66)) {
+        return "paper";
+    }
+    return "scissors";
+}
