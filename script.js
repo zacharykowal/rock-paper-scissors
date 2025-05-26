607b7ef22bc0d10539e487e588a73dc78b90543d
Zachary Kowal <zacharykowal31@gmail.com>
2025-05-26 12:24:11 -0400

    Create playGame function
    Move the score tracking system inside of the playGame function. As a
    result, the playRound function now returns the result of the current
    round instead of editing the global score variables.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const NUMBER_OF_ROUNDS = 5;
+const HUMAN_WIN = 1;
+const COMPUTER_WIN = 2;
+const TIE = 3;
+
 function getComputerChoice() {
-    let randomNumber = Math.random();
-    console.log(randomNumber);
+    const randomNumber = Math.random();
     if ((randomNumber >= 0) && (randomNumber < 0.33)) {
         return "rock";
     } else if ((randomNumber >= 0.33) && (randomNumber < 0.66)) {
@@ -16,33 +20,55 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     const humanChoiceLower = humanChoice.toLowerCase();
     if (humanChoiceLower === computerChoice) {
-        console.log(`It's a tie! You both chose ${humanChoiceLower}.`);
+        console.log(`This round is a tie. You both chose ${humanChoiceLower}.`);
+        return TIE;
     } else if (computerChoice === "rock") {
         if (humanChoiceLower === "paper") {
-            console.log("You win! Paper beats rock.");
-            humanScore += 1;
+            console.log("You win this round! Paper beats rock.");
+            return HUMAN_WIN;
         } else {
-            console.log("You lose. Rock beats scissors.");
-            computerScore += 1;
+            console.log("You lose this round. Rock beats scissors.");
+            return COMPUTER_WIN;
         }
     } else if (computerChoice === "paper") {
         if (humanChoiceLower === "scissors") {
-            console.log("You win! Scissors beats paper.");
-            humanScore += 1;
+            console.log("You win this round! Scissors beats paper.");
+            return HUMAN_WIN;
         } else {
-            console.log("You lose. Paper beats rock.");
-            computerScore += 1;
+            console.log("You lose this round. Paper beats rock.");
+            return COMPUTER_WIN;
         }
     } else {
         if (humanChoiceLower === "rock") {
-            console.log("You win! Rock beats scissors.");
-            humanScore += 1;
+            console.log("You win this round! Rock beats scissors.");
+            return HUMAN_WIN;
         } else {
-            console.log("You lose. Scissors beats paper.");
-            computerScore += 1;
+            console.log("You lose this round. Scissors beats paper.");
+            return COMPUTER_WIN;
         }
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < NUMBER_OF_ROUNDS; i++) {
+        let currentRoundResult = playRound(getHumanChoice(), getComputerChoice());
+        if (currentRoundResult === HUMAN_WIN) {
+            humanScore += 1;
+        } else if (currentRoundResult === COMPUTER_WIN) {
+            computerScore += 1;
+        }
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You win the game! The score was ${humanScore} to ${computerScore}.`);
+    } else if (humanScore < computerScore) {
+        console.log(`You lose the game. The score was ${computerScore} to ${humanScore}.`);
+    } else {
+        console.log(`The game was a tie. The score was ${humanScore} to ${computerScore}.`);
+    }
+}
+
+playGame();
