eb745f138575ecba3398eb713eaccfeefb74669a
Zachary Kowal <zacharykowal31@gmail.com>
2025-05-26 11:26:01 -0400

    Create play round function

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Choose rock, paper, or scissors:");
 }
+
+function playRound(humanChoice, computerChoice) {
+    const humanChoiceLower = humanChoice.toLowerCase();
+    if (humanChoiceLower === computerChoice) {
+        console.log(`It's a tie! You both chose ${humanChoiceLower}.`);
+    } else if (computerChoice === "rock") {
+        if (humanChoiceLower === "paper") {
+            console.log("You win! Paper beats rock.");
+            humanScore += 1;
+        } else {
+            console.log("You lose. Rock beats scissors.");
+            computerScore += 1;
+        }
+    } else if (computerChoice === "paper") {
+        if (humanChoiceLower === "scissors") {
+            console.log("You win! Scissors beats paper.");
+            humanScore += 1;
+        } else {
+            console.log("You lose. Paper beats rock.");
+            computerScore += 1;
+        }
+    } else {
+        if (humanChoiceLower === "rock") {
+            console.log("You win! Rock beats scissors.");
+            humanScore += 1;
+        } else {
+            console.log("You lose. Scissors beats paper.");
+            computerScore += 1;
+        }
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
