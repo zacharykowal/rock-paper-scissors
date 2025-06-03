6e35185f8ffb3615e3b76bf4f3e404a06444604b
Zachary Kowal <zacharykowal31@gmail.com>
2025-06-02 20:34:22 -0400

    Add UI to the game
    Rework game to be UI-based instead of console-based. Playing rounds are
    now a result of the user clicking a button. Display HTML text with the
    game/round results. Game now ends when somebody scores 5 instead of
    5 rounds flat.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const NUMBER_OF_ROUNDS = 5;
-const HUMAN_WIN = 1;
-const COMPUTER_WIN = 2;
-const TIE = 3;
+let humanScore = 0;
+let computerScore = 0;
+
+const roundResult = document.querySelector("#round-result");
+const gameResult = document.querySelector("#game-result");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.textContent.toLowerCase(), getComputerChoice());
+    });
+});
 
 function getComputerChoice() {
     const randomNumber = Math.random();
@@ -13,62 +20,44 @@ function getComputerChoice() {
     return "scissors";
 }
 
-function getHumanChoice() {
-    return prompt("Choose rock, paper, or scissors:");
-}
-
 function playRound(humanChoice, computerChoice) {
-    const humanChoiceLower = humanChoice.toLowerCase();
-    if (humanChoiceLower === computerChoice) {
-        console.log(`This round is a tie. You both chose ${humanChoiceLower}.`);
-        return TIE;
+    if (humanChoice === computerChoice) {
+        roundResult.textContent = `This round is a tie. You both chose ${humanChoice}.`;
     } else if (computerChoice === "rock") {
-        if (humanChoiceLower === "paper") {
-            console.log("You win this round! Paper beats rock.");
-            return HUMAN_WIN;
+        if (humanChoice === "paper") {
+            roundResult.textContent = "You win this round! Paper beats rock.";
+            humanScore += 1;
         } else {
-            console.log("You lose this round. Rock beats scissors.");
-            return COMPUTER_WIN;
+            roundResult.textContent = "You lose this round. Rock beats scissors.";
+            computerScore +=1;
         }
     } else if (computerChoice === "paper") {
-        if (humanChoiceLower === "scissors") {
-            console.log("You win this round! Scissors beats paper.");
-            return HUMAN_WIN;
+        if (humanChoice === "scissors") {
+            roundResult.textContent = "You win this round! Scissors beats paper.";
+            humanScore += 1;
         } else {
-            console.log("You lose this round. Paper beats rock.");
-            return COMPUTER_WIN;
+            roundResult.textContent = "You lose this round. Paper beats rock.";
+            computerScore += 1;
         }
     } else {
-        if (humanChoiceLower === "rock") {
-            console.log("You win this round! Rock beats scissors.");
-            return HUMAN_WIN;
-        } else {
-            console.log("You lose this round. Scissors beats paper.");
-            return COMPUTER_WIN;
-        }
-    }
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < NUMBER_OF_ROUNDS; i++) {
-        let currentRoundResult = playRound(getHumanChoice(), getComputerChoice());
-        if (currentRoundResult === HUMAN_WIN) {
+        if (humanChoice === "rock") {
+            roundResult.textContent = "You win this round! Rock beats scissors.";
             humanScore += 1;
-        } else if (currentRoundResult === COMPUTER_WIN) {
+        } else {
+            roundResult.textContent = "You lose this round. Scissors beats paper.";
             computerScore += 1;
         }
     }
 
-    if (humanScore > computerScore) {
-        console.log(`You win the game! The score was ${humanScore} to ${computerScore}.`);
-    } else if (humanScore < computerScore) {
-        console.log(`You lose the game. The score was ${computerScore} to ${humanScore}.`);
+    if (humanScore === 5) {
+        gameResult.textContent = `You win the game! The score was ${humanScore} to ${computerScore}. Click a button to play again.`;
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        gameResult.textContent = `You lose the game. The score was ${computerScore} to ${humanScore}. Click a button to play again.`;
+        humanScore = 0;
+        computerScore = 0;
     } else {
-        console.log(`The game was a tie. The score was ${humanScore} to ${computerScore}.`);
+        gameResult.textContent = `Human score: ${humanScore} Computer score: ${computerScore}`;
     }
 }
-
-playGame();
